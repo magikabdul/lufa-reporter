8a42b9a16620b8ff4bf6a00d9b58e82a98171246
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2025-10-03 19:22:26 +0200

    Configure the test profile in `application.yaml` and activate it in test class.

--- src/test/java/cloud/cholewa/lufa/reporter/LufaReporterApplicationTests.java
+++ src/test/java/cloud/cholewa/lufa/reporter/LufaReporterApplicationTests.java
@@ -2,8 +2,10 @@ package cloud.cholewa.lufa.reporter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LufaReporterApplicationTests {
 
     @Test

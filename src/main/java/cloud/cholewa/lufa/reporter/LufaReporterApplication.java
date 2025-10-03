baab7adc8bd3852fe0c8e89239396b8b1582742e
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2025-10-03 18:16:06 +0200

    Initialize Spring Boot project with Maven.
    - Add `.gitattributes` and `.gitignore` for development environment settings.
    - Configure `application.yaml` with application name.
    - Create main application class `LufaReporterApplication` and a basic test class.
    - Add Maven dependencies and plugins in `pom.xml` for Spring Boot, R2DBC, Flyway, and Postgres.

---
+++ src/main/java/cloud/cholewa/lufa/reporter/LufaReporterApplication.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.lufa.reporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LufaReporterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LufaReporterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cloud/cholewa/lufa/reporter/LufaReporterApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.lufa.reporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LufaReporterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

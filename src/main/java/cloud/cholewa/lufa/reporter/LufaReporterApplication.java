3932d6bd8dbf091ab50ae5de31efc0c8bcf66a36
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2025-10-03 23:19:07 +0200

    Refactor bot activation logic into `BotActivator` component.
    - Move `LufaBot` initialization to `BotActivator` and bind it to `ApplicationReadyEvent`.
    - Adjust `application.yaml` database `enabled` property to boolean format.
    - Minor comment correction in `pom.xml`.

--- src/main/java/cloud/cholewa/lufa/reporter/LufaReporterApplication.java
+++ src/main/java/cloud/cholewa/lufa/reporter/LufaReporterApplication.java
@@ -1,26 +1,12 @@
 package cloud.cholewa.lufa.reporter;
 
-import cloud.cholewa.lufa.reporter.telegram.LufaBot;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 
-@RequiredArgsConstructor
 @SpringBootApplication
 public class LufaReporterApplication {
 
-    private final LufaBot lufaBot;
-
     public static void main(String[] args) {
         SpringApplication.run(LufaReporterApplication.class, args);
-
-
-    }
-
-    @EventListener(ApplicationReadyEvent.class)
-    void startBot() {
-        lufaBot.start();
     }
 }
---
+++ src/main/java/cloud/cholewa/lufa/reporter/telegram/BotActivator.java
@@ -0,0 +1,20 @@
+package cloud.cholewa.lufa.reporter.telegram;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("reporter")
+@RequiredArgsConstructor
+public class BotActivator {
+
+    private final LufaBot lufaBot;
+
+    @EventListener(ApplicationReadyEvent.class)
+    void startBot() {
+        lufaBot.start();
+    }
+}

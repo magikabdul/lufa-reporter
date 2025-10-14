4f5c2af09a971a90e2950945255f71111af113db
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2025-10-14 09:22:53 +0200

    Enhance `LufaController` to include detailed response for empty report data. Use `HttpStatus.OK` with descriptive message when no records found for the requested year and month.

--- src/main/java/cloud/cholewa/reporter/report/lufa/api/LufaController.java
+++ src/main/java/cloud/cholewa/reporter/report/lufa/api/LufaController.java
@@ -3,6 +3,7 @@ package cloud.cholewa.reporter.report.lufa.api;
 import cloud.cholewa.reporter.report.lufa.service.LufaReportService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +29,10 @@ public class LufaController {
         return lufaReportService.prepareReport(year, month)
             .filter(reports -> !reports.isEmpty())
             .map(ResponseEntity::ok)
-            .switchIfEmpty(Mono.just(ResponseEntity.noContent().build()));
+            .switchIfEmpty(Mono.just(ResponseEntity.status(HttpStatus.OK)
+                .body(List.of(LufaReportResponse.builder()
+                    .description("No records in data base for: " + year + "-" + month)
+                    .build()))
+            ));
     }
 }
